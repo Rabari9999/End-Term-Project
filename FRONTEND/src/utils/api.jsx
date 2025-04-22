8533e627c09e6700abc097c045ae3fa5298f9d41
RABARI KRISH <rabarikrishramjibhai2905@gmail.com>
2025-04-22 19:09:15 +0530

    Update api.jsx

--- FRONTEND/src/utils/api.jsx
+++ FRONTEND/src/utils/api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 // Create an axios instance with your backend base URL
 const API = axios.create({
     baseURL:'https://end-term-project-6t71.onrender.com/api' // Change this if you deploy your backend
-// })
+});
 
 // Add a JWT token to every request if it exists in localStorage
 API.interceptors.request.use((config)=>{
